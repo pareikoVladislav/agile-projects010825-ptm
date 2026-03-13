6aa2194ae8577d39cf4b49261d0541ef27465b7c
ITShock <lanovenkyi@gmail.com>
2026-03-13 12:33:01 +0100

    add new model projectfile

--- projects/models/__init__.py
+++ projects/models/__init__.py
@@ -1,3 +1,4 @@
 from projects.models.project import Project
 from projects.models.task import Task
 from projects.models.tag import Tag
+from projects.models.project_file import ProjectFile
--- projects/models/project.py
+++ projects/models/project.py
@@ -4,6 +4,10 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField(max_length=600)
+    files = models.ManyToManyField(
+        "ProjectFile", blank=True,
+        related_name="projects"
+    )
     created_at = models.DateField(
         auto_now_add=True
     )
